f7985d1136a3adafc3a0d6331aed35b1407750ee
Desktop <veniciodasilvaferreira9@gmail.com>
2026-03-09 21:28:18 -0300

    feat - new meotod exlcluir cliente
    feat - todo estrutura do controller

--- Front_Oficina/src/main/java/com/oficina/project/controller/Clientes/ExcluirClienteController.java
+++ Front_Oficina/src/main/java/com/oficina/project/controller/Clientes/ExcluirClienteController.java
@@ -4,10 +4,43 @@
  */
 package com.oficina.project.controller.Clientes;
 
+import com.oficina.project.service.CarrosService;
+import com.oficina.project.service.ClienteService;
+import com.oficina.project.view.TelaPrincipal.TelaPrincipal;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Venicio
  */
 public class ExcluirClienteController {
-    
+
+    private TelaPrincipal view;
+    private ClienteService service;
+
+    public ExcluirClienteController(TelaPrincipal view) {
+        this.view = view;
+        this.service = new ClienteService();
+        IniciarController();
+
+    }
+
+    private void IniciarController() {
+        //Reconhe o o metodo GET que recebe click no botao excluir do front 
+        //em seguida chama o metodo "ExcluirUsuario" atraves do click
+        view.getBtExcluirCliente().addActionListener(e -> ExcluirCliente());
+
+    }
+
+    private void ExcluirCliente() {
+        //Estudar melhor op metodo e otimizar o service
+        String IdCliente = view.GetIdUsuarioCliente().trim();
+        boolean sucesso = service.ExcluirCliente(IdCliente);
+        if (sucesso == false) {
+            JOptionPane.showMessageDialog(null, "Cliente Excluido com Sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Não foi possivel Excluir o Cliente");
+        }
+
+    }
 }
